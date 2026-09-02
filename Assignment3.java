f11a90ced1b587619ac28b32cf2de340525fb4f9
Lyca Valencia <ljvalenxia99@gmail.com>
2026-09-02 23:01:49 +0800

    Add Assignment3.java

---
+++ Assignment3.java
@@ -0,0 +1,18 @@
+class Assignment3 { // This line indicates the name of the class
+    public static void main(String[] args) { // This indicate the name of the method Assignment3 which is the main method
+
+        String myFirstName = "Lyca Jean"; // this variable store my first name
+        String myLastName = "Valencia"; // this variable store my last name
+        String myAddress = "Mapandan"; // this variable store my address
+        String myMobileNumber = "09915805296";// this variable store my mobile number 
+        int myAge = 18;// this variable store my age
+        float myGWA = 96.50f;// this variable store my gwa
+
+        System.out.println("Firstname: " + myFirstName); // this line display my first name
+        System.out.println("Lastname: " + myLastName); // this line display my last name
+        System.out.println("Address: " + myAddress); // this line display my address
+        System.out.println("Mobile Number: " + myMobileNumber); // this line display my mobile number
+        System.out.println("Age: " + myAge); // this line display my age
+        System.out.println("GWA: " + myGWA); // this line display my gwa
+    }
+}
